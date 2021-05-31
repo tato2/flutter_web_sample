fe0a7fc74143451be9dda2ed00e8a021b19fb625
tato2 <tamurintamurin@gmail.com>
2021-05-31 13:49:40 +0900

    DataTableページを修正

--- lib/datatable_page.dart
+++ lib/datatable_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
@@ -27,53 +29,118 @@ class Form extends StatefulWidget {
 }
 
 class _MyState extends State<Form> {
-  late Future _future; // late:遅延初期化
   List _dataTable = [];
+  late StreamController _itemsController; // late:遅延初期化
+
+  String _code = '';
 
   @override
   void initState() {
     super.initState();
-    _future = asyncGetData();
+    _itemsController = StreamController();
+    _itemsController.add(null); // null=loadingとみなす
+    asyncGetData(_code);
   }
 
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _future,
+    return Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            firstContainer(),
+            secondContainer(),
+          ],
+        ));
+  }
+
+  Widget firstContainer() {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Flexible(
+              flex: 2,
+              child: Text('証券コード'),
+            ),
+            Flexible(
+              flex: 2,
+              child: TextFormField(
+                onChanged: (e) {
+                  _handleChanged('code', e);
+                },
+                maxLength: 4,
+              ),
+            ),
+          ],
+        ),
+        Row(children: [
+          ElevatedButton(
+            onPressed: () {
+              _itemsController.add(null);
+              asyncGetData(_code);
+            },
+            child: Text('検索'),
+          ),
+        ]),
+      ],
+    );
+  }
+
+  Widget secondContainer() {
+    return StreamBuilder(
+        stream: _itemsController.stream,
         builder: (BuildContext ctx, AsyncSnapshot<dynamic> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
+          print('builder ' + snapshot.hasData.toString());
+
+          if (!snapshot.hasData) {
             return Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Text('3秒待ちます'),
+                  Text(''),
                   CircularProgressIndicator(),
+                  Text('3秒待ちます'),
                 ],
               ),
             );
           }
-          return SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: PaginatedDataTable(
-                rowsPerPage: 10,
-                columns: const <DataColumn>[
-                  DataColumn(label: Text('証券コード')),
-                  DataColumn(label: Text('証券名')),
-                  DataColumn(label: Text('市場')),
-                ],
-                source: MyDataSource(_dataTable),
-              ));
+
+          if (_dataTable.length == 0) {
+            return Text('データなし');
+          }
+
+          return PaginatedDataTable(
+            rowsPerPage: _dataTable.length > 10 ? 10 : _dataTable.length,
+            columns: const <DataColumn>[
+              DataColumn(label: Text('証券コード')),
+              DataColumn(label: Text('証券名')),
+              DataColumn(label: Text('市場')),
+            ],
+            source: MyDataSource(_dataTable),
+          );
         });
   }
 
-  Future<void> asyncGetData() async {
+  Future<void> asyncGetData(String code) async {
     // 3秒待つ
     await Future.delayed(Duration(seconds: 3));
 
     String text = await rootBundle.loadString('assets/list.json');
-    var data = json.decode(text);
-    setState(() {
-      _dataTable = data;
-    });
+    _dataTable = json.decode(text);
+
+    if (_code != '') {
+      _dataTable.removeWhere(
+          (element) => !element['code'].toString().startsWith(code));
+    }
+    _itemsController.add(_dataTable);
+  }
+
+  void _handleChanged(String control, String e) {
+    switch (control) {
+      case 'code':
+        _code = e;
+        break;
+    }
   }
 }
 
@@ -96,7 +163,7 @@ class MyDataSource extends DataTableSource {
   @override
   int get rowCount => _dataTable.length;
   @override
-  bool get isRowCountApproximate => true;
+  bool get isRowCountApproximate => false;
   @override
   int get selectedRowCount => 0;
 }
